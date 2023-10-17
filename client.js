f1a4b947af3d805692a281a48b02f31b3fadcd6a
Paul Formby <paulformby@icloud.com>
2023-10-17 23:15:27 +0000

    used Es6 shorthand export modules and import

---
+++ client.js
@@ -0,0 +1,31 @@
+// client.js
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541  // PORT number here,
+  });
+
+  conn.on("connect", () => {
+    console.log("u r connected");
+  });
+
+  conn.on('timeout', () => {
+    console.log("you ded cuz you idled");
+  });
+
+  // Handle incoming data
+  conn.on('data', (data) => {
+    console.log("Received data from the server:", data);
+    // You can process the data here or perform other actions as needed.
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,33 +1,6 @@
 // play.js
 
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541  // PORT number here,
-  });
-
-  conn.on("connect", () => {
-    console.log("u r connected");
-  });
-
-  conn.on('timeout', () => {
-    console.log("you ded cuz you idled");
-  });
-
-  // Handle incoming data
-  conn.on('data', (data) => {
-    console.log("Received data from the server:", data);
-    // You can process the data here or perform other actions as needed.
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const { connect } = require("./client"); // Import the connect function from client.js
 
 console.log("Connecting ...");
 connect();
