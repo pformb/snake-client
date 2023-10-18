33b04bba0ecd146a5fb27522276b0a5e73895a2a
Paul Formby <paulformby@icloud.com>
2023-10-18 22:04:49 +0000

    added key event listener for control c quit

--- play.js
+++ play.js
@@ -1,6 +1,25 @@
 // play.js
-
 const { connect } = require("./client"); // Import the connect function from client.js
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  // Pass the 'key' to handleUserInput
+  stdin.on("data", (key) => handleUserInput(key)); 
+  
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
