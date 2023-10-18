0f855d2ccf94b3785bb251fc95640eebbd0b3f1f
Paul Formby <paulformby@icloud.com>
2023-10-18 22:31:48 +0000

    created input file and used Es6 shorthand objects to require setuppInput into play.js

---
+++ input.js
@@ -0,0 +1,21 @@
+// input.js
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (key) => handleUserInput(key)); 
+  
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+module.exports = { setupInput };
+
